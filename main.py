3f3ad4b98ef549944af7bdacedbcddff1469ef81
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2021-12-09 21:41:28 -0600

    Perspective transformation

--- main.py
+++ main.py
@@ -46,9 +46,29 @@ class MainWidget(Widget):
             # self.line.points = [self.perspective_point_x, 0, self.perspective_point_x, self.perspective_point_y]
             for line in self.vertical_lines:
                 line_x = int(central_line_x + offset*spacing)
-                line.points = [line_x, 0, line_x, self.height]
+                x1, y1 = self.transform(line_x, 0)
+                x2, y2 = self.transform(line_x, self.height)
+                line.points = [x1, y1, x2, y2]
                 offset += 1
 
+    def transform(self, x, y):
+        # return self.transform_2D(x,y)
+        return self.transform_perspective(x,y)
+    
+    def transform_2D(self, x, y):
+        return int(x), int(y)
+    
+    def transform_perspective(self, x, y):
+        tr_y = y * self.perspective_point_y / self.height
+        if tr_y > self.perspective_point_y:
+            tr_y = self.perspective_point_y
+
+        diff_x = x - self.perspective_point_x
+        diff_y = self.perspective_point_y - tr_y
+        proportion_y = diff_y / self.perspective_point_y # 1 when diff_y == perspective_y 
+        tr_x = self.perspective_point_x + diff_x*proportion_y
+
+        return int(tr_x), int(tr_y)
 
 
 class GalaxyApp(App):
