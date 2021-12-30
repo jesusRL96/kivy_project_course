325e217afc7f6fd9bd74cd807ba8682305a76cd4
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2021-12-29 20:52:06 -0600

    Forward movement

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line
+from kivy.properties import Clock
 
 class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
@@ -16,21 +17,25 @@ class MainWidget(Widget):
     H_NB_LINES = 15
     H_LINES_SPACING = 0.1 # percentage in screen width
     horizontal_lines = []
+    current_offset_y = 0
+    SPEED = 4
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.init_vertical_lines()
         self.init_horizontal_lines()
+        Clock.schedule_interval(self.update, 1/60)
 
     def on_parent(self, widget, parent):
         print('Parent ',self.width, self.height)
 
     def on_size(self, *args):
-        print('Size ',self.width, self.height)
-        self.perspective_point_x = self.width / 2
-        self.perspective_point_y = self.height * 0.75
-        self.update_vertical_lines()
-        self.update_horizontal_lines()
+        # print('Size ',self.width, self.height)
+        # self.perspective_point_x = self.width / 2
+        # self.perspective_point_y = self.height * 0.75
+        # self.update_vertical_lines()
+        # self.update_horizontal_lines()
+        pass
 
     def on_perspective_point_x(self, widget, value):
         print('PX ',value)
@@ -73,7 +78,7 @@ class MainWidget(Widget):
         x_max = central_line_x - offset*spacing
         spacing_y = self.H_LINES_SPACING*self.height
         for i, line in enumerate(self.horizontal_lines):
-            line_y = 0 + i*spacing_y
+            line_y = i*spacing_y-self.current_offset_y
             x1, y1 = self.transform(x_min, line_y)
             x2, y2 = self.transform(x_max, line_y)
             line.points = [x1, y1, x2, y2]
@@ -92,11 +97,21 @@ class MainWidget(Widget):
 
         diff_x = x - self.perspective_point_x
         diff_y = self.perspective_point_y - lin_y
-        factor_y = diff_y / self.perspective_point_y # 1 when diff_y == perspective_y
+        factor_y = diff_y / self.perspective_point_y if self.perspective_point_y else 0 # 1 when diff_y == perspective_y
         factor_y = pow(factor_y, 4)
         tr_x = self.perspective_point_x + diff_x*factor_y
         tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
         return int(tr_x), int(tr_y)
+
+    def update(self, dt):
+        self.perspective_point_x = self.width / 2
+        self.perspective_point_y = self.height * 0.75
+        self.update_vertical_lines()
+        self.update_horizontal_lines()
+        self.current_offset_y += self.SPEED
+        spacing_y = self.H_LINES_SPACING*self.height
+        if self.current_offset_y >= spacing_y:
+            self.current_offset_y -= spacing_y
 
 
 class GalaxyApp(App):
